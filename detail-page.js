34aec64dcb334402efa6e3587c1ca753158bd525
EmineBC <eminebahce@gmail.com>
2019-01-08 11:27:41 +0100

    Display elements and reset form

--- detail-page.js
+++ detail-page.js
@@ -1,20 +1,27 @@
 function submitComment() {
-    //console.log('Hello');
+    // gather data
     const inputField = document.getElementById('name').value;
-    console.log(inputField);
+    //const name = inputField.value;
+    const textArea = document.getElementById('msg').value;
+    //const msg = textArea.value;
 
-    const inputTextarea = document.getElementById('msg').value;
-    console.log(inputTextarea);
-
+    // create the elements you need
     const comment = document.createElement('section');
-
     const h3 = document.createElement('h3');
-    h3.innerHTML = `${inputField} said`;
-
     const p = document.createElement('p');
-    p.innerHTML = `${inputTextarea}`;
 
+    // adjust the elements we created
+    h3.innerHTML = `${inputField} said:`;
+    p.innerHTML = textArea;
     comment.classList.add('comment');
     comment.appendChild(h3);
     comment.appendChild(p);
+
+    // display the elements on the page
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
+
+    // reset form values
+    inputField.value = null;
+    textArea.value = null;
 }
